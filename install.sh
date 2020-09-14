67e8e3ecbc5c73bee514edd09f386a746fe968c3
jeromescuggs <subspacer@gmail.com>
2020-09-14 05:25:31 +0000

    updated install script- fixed errors, added alt target

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ echo "
   ╚════╝  ╚═╝  ╚═╝ ╚═╝     ╚═╝  ╚═════╝  ╚═╝  ╚═╝ ╚═╝ 
 "
 sleep 1s
-echo "Setting root execution path from $($DIR)..."
+echo "Setting root execution path from $(echo $DIR)..."
 sleep 1s
 echo "installing config files..."
 sleep 1s
 
-echo "looking for any prior config files and storing backups in $($DIR)"
+echo "looking for any prior config files and storing backups in $(echo $DIR)"
 sleep 1s
 
 if [[ -e $HOME/.tmux.conf ]]; then
@@ -31,11 +31,17 @@ echo "installing configuration..."
 sleep 1s
 cp $DIR/tmux.conf $HOME/.tmux.conf
 echo "installing tmux.conf at .tmux.conf in user home dir..."
-cp $DIR/.tmux.conf.local $HOME/.tmux.conf.local
-echo "installing .tmux.conf.local in user home dir..."
-sleep 1s
 
-echo "setting up session management plugins in $($DIR)..."
+if [[ $1 == "alt" ]]; then 
+    cp $DIR/.tmux.conf.alt.local $HOME/.tmux.conf.local
+    echo "installing alternate .tmux.conf.local in user home dir..."
+	sleep 1s
+else
+    cp $DIR/.tmux.conf.local $HOME/.tmux.conf.local
+    echo "installing .tmux.conf.local in user home dir..."
+    sleep 1s
+fi
+echo "setting up session management plugins in $(echo $DIR)..."
 cd $DIR && git submodule update --init 
 echo "tmux-resurrection and tmux-continuum have been installed."
 sleep 2s
